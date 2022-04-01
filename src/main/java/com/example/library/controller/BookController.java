f0c81548db38269c3d1c11ecce50919b6d83258a
Maciej Chrescionko <m.chrescionko@gmail.com>
2022-04-01 13:06:18 +0200

    added userService and errorMessage

--- src/main/java/com/example/library/controller/BookController.java
+++ src/main/java/com/example/library/controller/BookController.java
@@ -1,7 +1,11 @@
 package com.example.library.controller;
+
 import com.example.library.model.Book;
+import com.example.library.model.User;
 import com.example.library.service.BookService;
+import com.example.library.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +14,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @AllArgsConstructor
 public class BookController {
     private BookService bookService;
+    private UserService userService;
 
     @GetMapping("/book")
-    String book(Model model, String id) throws Exception {
+    String book(String id, Model model, String message) throws Exception {
         Book book = bookService.SearchByID(id);
         model.addAttribute("book", book);
-        model.addAttribute("owners", bookService.searchOwners(book));
+        model.addAttribute("owners", bookService.searchOwnersWithoutLoggedUser(book, userService.loggedUser().getId()));
+        if (message != null) {
+            model.addAttribute("message", message);
+            return "bookWithMessage";
+        }
         return "book";
     }
 }
