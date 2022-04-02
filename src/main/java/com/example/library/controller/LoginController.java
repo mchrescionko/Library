6c6d4a0a75690d60aba20e0e7e1a681224fdbca2
Maciej Chrescionko <m.chrescionko@gmail.com>
2022-04-03 01:29:21 +0200

    delete login post method, delete UserRequest from attributes in login get method

--- src/main/java/com/example/library/controller/LoginController.java
+++ src/main/java/com/example/library/controller/LoginController.java
@@ -1,33 +1,14 @@
 package com.example.library.controller;
 
-import com.example.library.request.UserRequest;
-import com.example.library.service.LoginService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
-    private LoginService loginService;
-
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
-    }
 
     @GetMapping("/login")
-    String login(Model model){
-        model.addAttribute("request", new UserRequest());
+    String login(){
         System.out.println("works till here");
         return "login";
     }
-
-    @PostMapping("/login")
-    String loginPost(@ModelAttribute("request") UserRequest userRequest){
-//        UserRequest userRequest = new UserRequest(email, password);
-        loginService.loginLogic(userRequest);
-        return "index";
-    }
-
 }
