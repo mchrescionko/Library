38abf27c5d77b08dab02dd10807e9263e00c8ea2
Maciej Chrescionko <m.chrescionko@gmail.com>
2022-04-03 01:21:23 +0200

    delete bookRepository, add try catch to method createExchange

--- src/main/java/com/example/library/controller/ExchangeController.java
+++ src/main/java/com/example/library/controller/ExchangeController.java
@@ -1,52 +1,55 @@
 package com.example.library.controller;
 
 import com.example.library.model.Book;
-import com.example.library.model.User;
-import com.example.library.repository.BookRepository;
 import com.example.library.service.ExchangeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import java.util.List;
 
-//zmiana url na bardziej przjerzyste
 @Controller()
 @AllArgsConstructor
 public class ExchangeController {
 
     private ExchangeService exchangeService;
-    private BookRepository bookRepository;
 
-    @PostMapping("/createExchange")
-    String createExchange(String receiverBookId, Integer receiverId){
-        exchangeService.createExchange(receiverBookId, receiverId);
+    @PostMapping("/askForSwap")
+    String createExchange(Model model, String receiverBookId, Integer receiverId) {
+        try {
+            exchangeService.createExchange(receiverBookId, receiverId);
+        } catch (RuntimeException e) {
+            model.addAttribute("message", "You already asked for this book!");
+            System.out.println("message: " + e.getMessage());
+            model.addAttribute("id", receiverBookId);
+            return "redirect:book";
+        }
         return "redirect:book?id=" + receiverBookId;
     }
 
     @PostMapping("/denyExchange")
-    String denyRequest(Integer id){
+    String denyRequest(Integer id) {
         exchangeService.deleteExchange(id);
         return "redirect:bookShelf";
     }
 
     @PostMapping("/choseSenderBook")
-    String choseABook(Model model, Integer exchangeId){
+    String choseABook(Model model, Integer exchangeId) {
         List<Book> books = exchangeService.getSenderBooks(exchangeId);
         model.addAttribute("books", books);
         model.addAttribute("exchangeId", exchangeId);
         return "senderBookShelf";
     }
+
     @PostMapping("/chooseSenderBook")
-    String choseSenderBook(Integer exchangeId, String senderBookId){
-        exchangeService.setSecondStep(exchangeId,senderBookId);
-        return "redirect:bookShelf";
-    }
-    @PostMapping("/finalAccept")
-    String finalAccept(Integer exchangeId){
-        exchangeService.finalAccept(exchangeId);
+    String choseSenderBook(Integer exchangeId, String senderBookId) {
+        exchangeService.setSecondStep(exchangeId, senderBookId);
         return "redirect:bookShelf";
     }
 
+    @PostMapping("/finalAccept")
+    String finalAccept(Integer exchangeId) {
+        exchangeService.finalAccept(exchangeId);
+        return "redirect:bookShelf";
+    }
 }
